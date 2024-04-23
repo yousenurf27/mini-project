21643c38cbbe9c28462d32d023530b69d453040f
yousenurf <yosenurf@gmail.com>
2024-04-24 05:16:23 +0700

    chore: add logger use winston

--- apps/api/src/prisma.ts
+++ apps/api/src/prisma.ts
@@ -1,3 +1,41 @@
 import { PrismaClient } from '@prisma/client';
+import { logger } from './logging';
 
-export default new PrismaClient({ log: ['query', 'info', 'warn', 'error'] });
+const prisma =  new PrismaClient({
+  log: [
+    {
+      emit: 'event',
+      level: 'query'
+    },
+    {
+      emit: 'event',
+      level: 'error'
+    },
+    {
+      emit: 'event',
+      level: 'info'
+    },
+    {
+      emit: 'event',
+      level: 'warn'
+    },
+  ]
+});
+
+export default prisma
+
+prisma.$on('error', (e) => {
+  logger.error(e);
+})
+
+prisma.$on('query', (e) => {
+  logger.info(e);
+})
+
+prisma.$on('info', (e) => {
+  logger.info(e);
+})
+
+prisma.$on('warn', (e) => {
+  logger.warn(e);
+})
